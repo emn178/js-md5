da009f933f2591aa9c130f86180ccc44d56619d4
Chen Yi-Cyuan <emn178@gmail.com>
2016-07-30 20:39:20 +0800

    update comment version

--- src/md5.js
+++ src/md5.js
@@ -2,7 +2,7 @@
  * [js-md5]{@link https://github.com/emn178/js-md5}
  *
  * @namespace md5
- * @version 0.4.0
+ * @version 0.4.1
  * @author Chen, Yi-Cyuan [emn178@gmail.com]
  * @copyright Chen, Yi-Cyuan 2014-2016
  * @license MIT
